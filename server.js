2947af37808d2bd2e0003ae8f4c0c714ecaeedbb
Guitarly Nguyen <guitarly@gmail.com>
2018-03-17 20:21:11 -0400

    Testing

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
 
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
-  next();
+    console.log(log);
+    fs.appendFile('server.log', log + '\n');
+    next();
 });
 
 // app.use((req, res, next) => {
@@ -24,33 +24,33 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
+    return new Date().getFullYear();
 });
 
 hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
+    return text.toUpperCase();
 });
 
 app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website'
+    });
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page'
-  });
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
 });
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+    console.log(`Server is up on port ${port} testing`);
 });
